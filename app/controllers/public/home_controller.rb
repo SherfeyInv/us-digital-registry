414e11018c90bfbc32c39ad3748837c6c9bd367a
Ryan Wold <ryan.wold@gsa.gov>
2022-11-18 13:39:27 -0800

    redirect to Touchpoints

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -1,14 +1,14 @@
 class Public::HomeController < ApplicationController
 
   def index
-
+    redirect_to "https://touchpoints.app.cloud.gov/registry"
   end
 
   def federalagencies
-
+    redirect_to "https://touchpoints.app.cloud.gov/registry"
   end
 
   def developers
-
+    redirect_to "https://touchpoints.app.cloud.gov/registry"
   end
 end
